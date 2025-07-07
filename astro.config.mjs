e2351435b95548d19db0ef4220034df37132835c
danilosimonatto <danilo.simonatto@icloud.com>
2025-07-07 22:50:34 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import robots from "astro-robots";
+import unfonts from "unplugin-fonts/astro"; // optional
+import netlify from "@astrojs/netlify"; // optional
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://example.com", // ← update later
+	output: "static",
+	integrations: [
+		vue(),
+		tailwind(),
+		compress({ html: true, img: true }),
+		sitemap(),
+		robots(),
+		unfonts({
+			google: { families: ["Inter:400,700"] }, // sample config
+		}),
+	],
+	adapter: netlify(),
+});
